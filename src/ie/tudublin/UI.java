81bfd9ca0459dc81ad61865a2e55564d2185949a
Gediminas Petreikis <C17323756@mydit.ie>
2019-04-10 18:01:28 +0100

    finished with the resistor drawing

--- src/ie/tudublin/UI.java
+++ src/ie/tudublin/UI.java
@@ -16,6 +16,9 @@ public class UI extends PApplet
 		int hundreds = (value / 100);
 		int tens = (value - (hundreds * 100)) / 10;
 		int ones = value - ((hundreds * 100)  + (tens * 10));
+		int val1 = 0;
+		int val2 = 0;
+		int val4 = 0;
 		print(hundreds + ",");
 		print(tens + ",");
 		println(ones);
@@ -65,6 +68,18 @@ public class UI extends PApplet
 		//drawing resistors lines
 		line(200, 150, 300, 150);
 		line(400, 150, 500, 150);
+		//-----------------------
+		line(200, 350, 300, 350);
+		line(400, 350, 500, 350);
+		//-----------------------
+		line(200, 550, 300, 550);
+		line(400, 550, 500, 550);
+		//text showing the resistor values
+		textSize(20);
+		fill(0);
+		text("val1" ,700 , 150 );
+		text("val2" ,700 , 350 );
+		text("val3" ,700 , 550 );
 	}
 
 
